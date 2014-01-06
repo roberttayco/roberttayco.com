17eb3c2043ab53826eea64ecd510555c9ee14435
Robert Tayco <rob@roberttayco.com>
2014-01-05 17:51:14 -0800

    initial commit
    Beginning of the jekyll-ified portfolio site. Figuring out partials.

---
+++ Gruntfile.js
@@ -0,0 +1,67 @@
+module.exports = function(grunt) {
+   grunt.initConfig({
+      pkg: grunt.file.readJSON('package.JSON'),
+
+      cssOutput: './css/style.css',
+      sassOutput: './css/sass/style.scss',
+      sassPath: './css/sass/**/*.scss',
+      jekyllPath: [
+         './*.html',
+         './_includes/*.html',
+         './_layouts/*.html',
+         './_posts/*.html'
+         ],
+
+      sass: {
+         dist: {
+            files: {
+               '<%= cssOutput %>':'<%= sassOutput %>'
+            },
+            options: {
+               outputStyle: 'compressed'
+            }
+         },
+         dev: {
+            files: {
+               '<%= cssOutput %>':'<%= sassOutput %>'
+            },
+            options: {
+               includePaths: ['<%= sassPath %>'],
+               outputStyle: 'expanded',
+               sourceComments: 'normal'
+            }
+         }
+      },
+
+      jekyll: {
+         dist: {
+            options: {}
+         }
+      },
+
+      watch: {
+         html: {
+            files: ['<%= jekyllPath %>'],
+            tasks: ['jekyll:dist'],
+            options: {
+               livereload: true
+            }
+         },
+         css: {
+            files: ['<%= sassPath %>'],
+            tasks: ['sass:dev'],
+            options: {
+               livereload: true,
+               spawn: false
+            }
+         }
+      }
+   });
+
+   grunt.loadNpmTasks('grunt-sass');
+   grunt.loadNpmTasks('grunt-jekyll');
+   grunt.loadNpmTasks('grunt-contrib-watch');
+
+   grunt.registerTask('default', ['sass:dev']);
+   grunt.registerTask('release', ['sass:dist']);
+};
