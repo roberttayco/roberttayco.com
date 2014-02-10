8c5d729871ae1ed0033202c7adff68c847c98998
Robert Tayco <rob@roberttayco.com>
2014-02-09 22:52:37 -0800

    building gulpfile

--- _site/gulpfile.js
+++ _site/gulpfile.js
@@ -1,20 +1,51 @@
-var gulp  = require('gulp');
-var gutil = require('gulp-util');
-var sass  = require('gulp-sass');
+var gulp    = require('gulp'),
+    sass    = require('gulp-sass'),
+    jekyll  = require('gulp-jekyll'),
+    refresh = require('gulp-livereload'),
+    lr      = require('tiny-lr'),
+    server  = lr();
+
+var paths = {
+   sass: 'css/sass/**/*.scss',
+   markupSrc:  [
+      '*.html',
+      '_layouts/**',
+      '_posts/**',
+      '_includes/**'
+   ],
+   markupDir: './',
+   markupDest: '_site'
+};
+
+gulp.task('livereload', function() {
+   server.listen(35729, function(err) {
+      if (err) return console.log(err);
+   });
+});
 
 gulp.task('sass', function() {
-   gulp.src('css/sass/style.scss')
+   return gulp.src('css/sass/style.scss')
       .pipe(sass({
          includePaths: ['css/sass'],
          outputStyle: 'compressed'
       }))
-      .pipe(gulp.dest('css'));
+      .pipe(gulp.dest('css'))
+      .pipe(refresh(server));
 });
 
-
-gulp.task('default', function() {
-   gulp.run('sass');
-   gulp.watch(['css/sass/**/*.scss'], function() {
-      gulp.run('sass');
-   });
+gulp.task('jekyll', function() {
+   return gulp.src(paths.markupDir)
+      .pipe(jekyll({
+         source: './',
+         destination: paths.markupDest,
+      }))
+      .pipe(gulp.dest('./_site/'))
+      .pipe(refresh(server));
 });
+
+gulp.task('watch', function() {
+   gulp.watch(paths.sass, ['sass']);
+   gulp.watch(paths.markupSrc, ['jekyll']);
+});
+
+gulp.task('default', ['sass', 'jekyll', 'livereload', 'watch']);
--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,74 @@
-var gulp  = require('gulp');
-var gutil = require('gulp-util');
-var sass  = require('gulp-sass');
+var gulp       = require('gulp'),
+    sass       = require('gulp-sass'),
+    prefix     = require('gulp-autoprefixer'),
+    jekyll     = require('gulp-jekyll'),
+    imagemin   = require('gulp-imagemin'),
+    livereload = require('gulp-livereload'),
+    lr         = require('tiny-lr'),
+    server     = lr();
 
-gulp.task('sass', function() {
-   gulp.src('css/sass/style.scss')
-      .pipe(sass({
-         includePaths: ['css/sass'],
-         outputStyle: 'compressed'
-      }))
-      .pipe(gulp.dest('css'));
-});
+var paths = {
+   markupSrc:  [
+      '*.html',
+      '_layouts/**',
+      '_posts/**',
+      '_includes/**'
+   ],
+   markupDir: './',
+   markupDest: '_site'
+};
 
 
-gulp.task('default', function() {
-   gulp.run('sass');
-   gulp.watch(['css/sass/**/*.scss'], function() {
-      gulp.run('sass');
+var jekyll = {
+  src:      '*.html',
+  layouts:  '_layouts/*',
+  includes: '_includes/*',
+  posts:    '_posts/*',
+  build:    '_site'
+};
+
+var dest = {
+  css:   jekyll.build + '/css',
+  img:   jekyll.build + '/img',
+  js:    jekyll.build + '/js'
+};
+
+var sass = {
+  src:     'css/sass/style.scss',
+  imports: 'css/sass/**',
+  dest:    'css'
+};
+
+gulp.task('livereload', function() {
+   server.listen(35729, function(err) {
+      if (err) return console.log(err);
    });
 });
+
+gulp.task('sass', function() {
+   return gulp.src(sass.src)
+      .pipe(sass({
+         includePaths: [sass.imports],
+         outputStyle:  'expanded',
+         lineNumbers:  true
+      }))
+      .pipe(gulp.dest(sass.dest))
+      .pipe(livereload(server));
+});
+
+// gulp.task('jekyll', function() {
+//    return gulp.src(paths.markupDir)
+//       .pipe(jekyll({
+//          source: './',
+//          destination: paths.markupDest,
+//       }))
+//       .pipe(gulp.dest('./_site/'))
+//       .pipe(refresh(server));
+// });
+
+gulp.task('watch', function() {
+   gulp.watch(paths.sass, ['sass']);
+   gulp.watch(paths.markupSrc, ['jekyll']);
+});
+
+gulp.task('default', ['sass', 'livereload', 'watch']);
